f7d6db6205225f2000cde18ed3c681ea4a75121a
Jaj <codewithjom@gmail.com>
2025-05-07 22:07:26 +0800

    feat: added login form.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import LoginForm from '@/components/pages/auth/login-form'
+
 export default function Home() {
   return (
-    <main>
-      <h1>Hello</h1>
-    </main>
+    <div className='bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10'>
+      <div className='w-full max-w-sm md:max-w-3xl'>
+        <LoginForm />
+      </div>
+    </div>
   )
 }
